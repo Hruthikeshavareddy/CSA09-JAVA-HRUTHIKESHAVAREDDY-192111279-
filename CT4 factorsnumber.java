ce8875e8462c4d7c020ca5e9ee22f4ef87ef2f98
Hruthikeshavareddy <113327069+Hruthikeshavareddy@users.noreply.github.com>
2024-01-30 08:15:11 +0530

    Add files via upload

---
+++ CT4 factorsnumber.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class factorsnumber
+{
+	public static void main(String[] args) 
+	{
+		Scanner s=new Scanner(System.in);
+		System.out.println("Given number:");
+		int num=s.nextInt();
+		System.out.println("N:");
+		int n=s.nextInt();
+		int a[]=new int[100];
+		int x=0;
+		for(int i=1;i<=num;i++)
+		{
+			if(num%i==0){
+			
+				a[x]=i;
+				x++;
+			}
+		}
+		System.out.println("Number of factors:"+x);
+		System.out.println(n+"th factor of"+num+"="+a[n-1]);	
+	}
+}
